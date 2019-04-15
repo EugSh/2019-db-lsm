cc45e163ca352bcb73753359fad5eff8dcbb8a9a
EugSh <eug.shkalev@yandex.ru>
2019-04-15 20:22:08 +0300

    fix

--- src/main/java/ru/mail/polis/MyDAO.java
+++ src/main/java/ru/mail/polis/MyDAO.java
@@ -9,12 +9,12 @@ import java.util.TreeMap;
 import org.jetbrains.annotations.NotNull;
 
 public class MyDAO implements DAO {
-    
+
     private final NavigableMap<ByteBuffer, Record> dataBase = new TreeMap<>();
 
     @NotNull
     @Override
-    public Iterator<Record> iterator(@NotNull ByteBuffer from) throws IOException {
+    public Iterator<Record> iterator(@NotNull final ByteBuffer from) throws IOException {
         return dataBase.tailMap(from).values().iterator();
     }
 
@@ -28,17 +28,6 @@ public class MyDAO implements DAO {
         dataBase.remove(key);
     }
 
-    /**
-     * Closes this stream and releases any system resources associated
-     * with it. If the stream is already closed then invoking this
-     * method has no effect.
-     * <p> As noted in {@link AutoCloseable#close()}, cases where the
-     * close may fail require careful attention. It is strongly advised
-     * to relinquish the underlying resources and to internally
-     * <em>mark</em> the {@code Closeable} as closed, prior to throwing
-     * the {@code IOException}.
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     public void close() throws IOException {
         //nothing
